422852e14dd89ff8b3f658b7230d460bac886240
benard <benardboiwo703@gmail.com>
2024-07-08 01:35:25 +0300

    Updated various files across different directories

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+// Function to generate thank you messages
+function writeCards(namesArray, eventName) {
+    let thankYouMessages = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      thankYouMessages.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  // Function to count down from a number to zero
+  function countDown(startingNumber) {
+    let count = startingNumber;
+    while (count >= 0) {
+      console.log(count);
+      count--;
+    }
+  }
+  
